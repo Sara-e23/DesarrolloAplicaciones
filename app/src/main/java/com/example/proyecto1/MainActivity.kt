c14521f338b95c63cc3167cc09cd17361f34d21a
Sara-e23 <s.eliza0423@gmail.com>
2025-02-12 07:52:53 -0600

    Box layout and Text properties

--- app/src/main/java/com/example/proyecto1/MainActivity.kt
+++ app/src/main/java/com/example/proyecto1/MainActivity.kt
@@ -4,16 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.proyecto1.ui.theme.Proyecto1Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,52 +32,93 @@ class MainActivity : ComponentActivity() {
         //enableEdgeToEdge()
         setContent {
             Proyecto1Theme {
-                Row(){//utilizaremos Row, Column y Box
+                Column(){//utilizaremos Row, Column y Box
                     Column(){
-                        Text(text = "minho")
-                        Text(text = "Welcome to your first app")
-                    }
-                    Column(){
-                        Text(text = "minho")
-                        Text(text = "Welcome to your first app")
-                    }
-                    Column(){
-                        Text(text = "minho")
-                        Text(text = "Welcome to your first app")
+                        TextComposable()
+                        TextComposable()
+                        TextComposable()
                     }
                 }
-                Text(text = "minsung")
-                Text(text = "Welcome to your first app")
-            //hay que poner layouts para no sobreponer objetos, tarea
-                //layouts en android compose 1 y 2 funciones composable
-                //se agregó la primera interfaz de texto en el archivo
-                //interfaz main activity
-                /*
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    }
-                }*/
+                Row(){
+                    TextComposable()
+                    TextComposable()
+                    TextComposable()
+
+                }
+                Column(){
+                    ModifierExample2()
+
+                    CustomText()
+                }
             }
         }
     }
-}
+    @Preview(showBackground = true)
 
-/*
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    fun TextComposable(name: String = "Empty"){
+        Text("Hello World")
+        Text(name)
+    }
+    @Preview(showBackground = true)
+    @Composable
+    fun ModifierExample1(){
+        Column(
+            modifier = Modifier
+                .padding(50.dp, 30.dp, 20.dp, 10.dp)
+        ){
+            Text("Hello World")
+        }
+    }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Proyecto1Theme {
-        Greeting("Android")
+fun ModifierExample2(){
+    Box(
+        modifier = Modifier
+            .background(Color.Blue)
+            .padding(10.dp)
+            .height(300.dp)
+            .width(300.dp)
+    ){
+        Text("1", Modifier.align(Alignment.TopStart))
+        Text("2", Modifier.align(Alignment.TopCenter))
+        Text("3", Modifier.align(Alignment.TopCenter))
     }
-  }
- */
+
+@Preview(showBackground = true)
+@Composable
+fun ModifierExample(){
+    Box(
+        modifier = Modifier
+            .background(Color.Blue)
+            .padding(10.dp)
+            .height(300.dp)
+            .width(300.dp)
+    ){
+        Text("1", Modifier.align(Alignment.TopStart))
+        Text("2", Modifier.align(Alignment.TopCenter))
+        Text("3", Modifier.align(Alignment.TopCenter))
+    }
+}
+}
+    @Preview(showBackground = true)
+    @Composable
+    fun CustomText(){
+        Column(){
+            Text(
+                StringResource(R.string.sample.text),
+                color = colorResource(R.color.purple_200),
+                fontSize = 20.sp,
+                fontStyle = FontStyle.Italic, //para cursivas
+                fontWeight = FontWeight.ExtraBold,
+            )
+            val gradientColors = listOff(Color.Blue, Color.Red, Color.Black, color = colorResource(R.color.purple_200))
+            Text(
+                StringResource(R.string.sample.text),
+                style = TextStyle(brush = Brush.linearGradient(colors = gradientColors))
+            )
+        }
+
+    }
+}
