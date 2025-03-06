aa2defb91c5e444f5ead69b5f954697ee5145bc5
Sara-e23 <s.eliza0423@gmail.com>
2025-03-05 21:04:53 -0600

    Barra de menú deslizable individual

--- app/src/main/java/com/example/proyecto1/ui/Screens/ComponentsScreen.kt
+++ app/src/main/java/com/example/proyecto1/ui/Screens/ComponentsScreen.kt
@@ -3,32 +3,56 @@ package com.example.proyecto1.ui.Screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.InputChip
+import androidx.compose.material3.InputChipDefaults
+import androidx.compose.material3.LargeFloatingActionButton
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderPositions
+import androidx.compose.material3.SmallFloatingActionButton
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -39,6 +63,11 @@ import java.lang.StackWalker.Option
 @Composable
 fun ComponentsScreen(navController: NavHostController){
     //Buttons()
+    //FloatingButtons()
+    //Progress()
+    //Chips()
+    //Sliders()
+    //Switches()
 
 
     var option by rememberSaveable { mutableStateOf("buttons") }
@@ -91,6 +120,19 @@ fun ComponentsScreen(navController: NavHostController){
                         }
                     }
                 )
+                NavigationDrawerItem(
+                    icon = { Icon(Icons.Filled.ShoppingCart, contentDescription = "") },
+                    label = { Text("Third Menu Item") },
+                    selected = false,
+                    onClick = {
+                        option = "Third"
+                        scope.launch {
+                            drawerState.apply {
+                                close()
+                            }
+                        }
+                    }
+                )
             }
         }
     ) {
@@ -111,7 +153,7 @@ fun ComponentsScreen(navController: NavHostController){
     }
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun Buttons(){
     Column(
@@ -135,5 +177,183 @@ fun Buttons(){
         TextButton(onClick = {}) {
             Text("Text")//texto botón
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FloatingButtons(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),//espacio 100% del dispositivo
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ){
+        FloatingActionButton(onClick = {}) {
+            Icon(Icons.Filled.Add, "Add button")
+        }
+        SmallFloatingActionButton(onClick = {}) {
+            Icon(Icons.Filled.Add, "Add button")
+        }
+        LargeFloatingActionButton(onClick = {}) {
+            Icon(Icons.Filled.Add, "Add button")
+        }
+        ExtendedFloatingActionButton(onClick = {}) {
+            Icon(Icons.Filled.Add, "Add button")
+            Text("Button")
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun Progress() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),//espacio 100% del dispositivo
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        LinearProgressIndicator(
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+        CircularProgressIndicator(
+            modifier = Modifier
+                .width(64.dp)
+        )
+    }
+}
+//@Preview(showBackground = true)
+@Composable
+fun Chips() {//etiquetas y acciones rápidas
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),//espacio 100% del dispositivo
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        AssistChip(
+            onClick = {},
+            label = { Text("Assist Chip") },
+            leadingIcon = {
+                Icon(Icons.Filled.AccountBox,
+                    contentDescription = "Assist Chip",
+                    modifier = Modifier
+                        .size(AssistChipDefaults.IconSize)
+                )
+            }
+        )//assistChip
+        var selected by remember { mutableStateOf(false) }
+        FilterChip(
+            selected = selected,//saber si el fitro está activo o no
+            onClick = { selected = !selected},
+            label = { Text("Filter Chip") },
+            leadingIcon = if(selected) {
+                {
+                    Icon(
+                        Icons.Filled.AccountBox,
+                        contentDescription = "Assist Chip",
+                        modifier = Modifier
+                            .size(AssistChipDefaults.IconSize)
+                    )
+                }
+            }else {
+                null
+            }
+        )
+        InputChipExample("Dismiss",{})
+    }
+}
+
+@Composable
+fun InputChipExample(
+    text: String,
+    onDismiss: () -> Unit
+){
+    var enabled by remember { mutableStateOf(true) }
+    if (!enabled) return
+    InputChip(
+        label = {Text(text)},
+        selected = enabled,
+        onClick = {
+            onDismiss()
+            enabled = !enabled
+        },
+        avatar = {
+            Icon(
+                Icons.Filled.Person,
+                contentDescription = "Icon Person",
+                Modifier.size(InputChipDefaults.AvatarSize)
+            )
+        },
+        trailingIcon = {
+            Icon(
+                Icons.Filled.Close,
+                contentDescription = "Icon Person",
+                Modifier.size(InputChipDefaults.AvatarSize)
+            )
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Sliders() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),//espacio 100% del dispositivo
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        var SliderPosition by remember { mutableStateOf(58f)}
+        Slider(
+            value = SliderPosition,
+            onValueChange = { SliderPosition = it},
+            steps = 10, //en que cantidades se va a mover y un rango minimo
+            valueRange = 0f .. 100f//rango de 0 a 100 en flotante
+        )
+        Text(
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth(),
+            text = SliderPosition.toString()//convertir flotante a string
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Switches() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),//espacio 100% del dispositivo
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        var checked by remember { mutableStateOf(true)}
+        Switch(
+            checked = checked,
+            onCheckedChange = { checked = it}//actualizando el valor de la var
+        )
+        var checked2 by remember { mutableStateOf(true) }
+        Switch(
+            checked = checked2,
+            onCheckedChange = { checked2 = it},//actualizando el valor de la var
+            thumbContent = if (checked2){
+                {
+                    Icon(
+                        Icons.Filled.Check,
+                        contentDescription = "Switch Check",
+                        Modifier.size(InputChipDefaults.AvatarSize)
+                    )
+                }
+            } else{
+                null
+            }
+        )
+        var checked3 by remember { mutableStateOf(true) }
+        Checkbox(
+            checked = checked3,
+            onCheckedChange = {checked3 = it}
+        )
     }
 }
